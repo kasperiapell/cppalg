32ecba0a4fb9e4b1ef618e3b64e41658b6411763
kasperiapell <kasperi.apell@gmail.com>
2023-09-01 11:46:27 +0300

    add array binary search

--- include/cppalg/cppalg.hpp
+++ include/cppalg/cppalg.hpp
@@ -1,4 +1,5 @@
 // include/cppalg.hpp
 
 #include "cppalg/linkedList.hpp"
+#include "cppalg/search.hpp"
 #include "cppalg/sort.hpp"
---
+++ include/cppalg/search.cpp
@@ -0,0 +1,26 @@
+// include/cppalg/search.hpp
+
+namespace search {
+	#include <vector>
+
+	// return -1 if target not found in the vector
+	int binarySearch(std::vector<int>& vec, int target) {
+		int bot = 0;
+        	int mid;
+        	int top = vec.size() - 1;
+
+	        while (bot <= top) {
+	        	mid = bot + (top - bot) / 2;
+	        	int val = vec[mid];
+
+	        	if (val == target) {
+	        		return mid;
+	        	} else if (val > target) {
+	        		top = mid - 1;
+	        	} else {
+	        		bot = mid + 1;
+	        	}
+	        }   
+	        return -1; 
+	}
+}
