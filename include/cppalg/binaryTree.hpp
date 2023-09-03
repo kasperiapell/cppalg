00909357b497d56d973516fdbdb236a8637f7cb4
kasperiapell <kasperi.apell@gmail.com>
2023-09-03 18:33:10 +0300

    add inorder traversal

--- include/cppalg/binaryTree.hpp
+++ include/cppalg/binaryTree.hpp
@@ -1,6 +1,20 @@
 // include/cppalg/binaryTree.hpp
 
 namespace binaryTree {
+
+	/*
+	 * The functions operate on a binary tree formed from nodes of the form
+	 *
+	 * 	struct Node {
+	 * 		int val;
+	 *		Node* left;
+	 *		Node* right;
+	 * 	}
+	 *
+	 */
+
+	#include <stack>
+
 	inline void invertChildren(Node* root) {
 		if (root) {
 			Node* tmep = root->left;
@@ -12,18 +26,27 @@ namespace binaryTree {
 		}
 	}
 
-	/*
-	 * Operates on a binary tree formed from nodes of the form
-	 *
-	 * 	struct Node {
-	 * 		int val;
-	 *		Node* left;
-	 *		Node* right;
-	 * 	}
-	 *
-	 */
 	inline Node* invert(Node* root) {
 		invertChildren(root);
 		return root;
 	}
+
+	inline void inorderTraversal(Node* root) {
+		vector<int> out;
+	        if (!root) return out;
+	        stack<TreeNode*> stack;
+
+	        while (root || !stack.empty()) {
+	        	while (root) {
+	                	stack.push(root);
+	                	root = root->left;
+	            	}
+	            	root = stack.top();
+	            	stack.pop();
+	            	out.push_back(root->val);
+	            	root = root->right;
+	        }
+	        return out; 
+	}
+
 }
