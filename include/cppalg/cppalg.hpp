c5a664c9820e050fa0ad5647ff4f67b16499aea7
kasperiapell <kasperi.apell@gmail.com>
2023-09-11 22:15:59 +0300

    add bit operations

--- include/cppalg/cppalg.hpp
+++ include/cppalg/cppalg.hpp
@@ -1,6 +1,7 @@
 // include/cppalg.hpp
 
 #include "cppalg/binaryTree.hpp"
+#include "cppalg/bitOperations.hpp"
 #include "cppalg/linkedList.hpp"
 #include "cppalg/search.hpp"
 #include "cppalg/sort.hpp"
