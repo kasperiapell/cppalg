8e77b8226dd83b4de0c21e673935ca1043134272
kasperiapell <kasperi.apell@gmail.com>
2023-09-18 12:52:51 +0300

    add generatePermutations

--- include/cppalg/vect.hpp
+++ include/cppalg/vect.hpp
@@ -2,8 +2,12 @@
 
 namespace vect {
 	#include <vector>
+	#include <numeric>
+	
+	#define MAX_PERMUTATION_SIZE 10
 
-	inline std::vector<std::vector<int>> powerSet(std::vector<int>& vec) {
+	inline std::vector<std::vector<int>> powerSet(std::vector<int>& vec)
+	{
 		vector<vector<int>> out = {{}};
 
 	        for (int num: vec) {
@@ -16,5 +20,31 @@ namespace vect {
 	        }
 
         	return out;
+	}
+
+	inline void backtrackPermutations(std::vector<int>& ints, std::vector<int>& vec, int k, std::vector<std::vector<int>>& out) {
+		if (k == ints.size()) {
+			out.push_back(vec);
+			return; 
+		}
+
+		for (int i = 0; i < ints.size(); i++) {
+			if (vec[i] == MAX_PERMUTATION_SIZE + 1) {
+		                vec[i] = ints[k];
+		                backtrackPermutations(ints, vec, k + 1, out);
+		                vec[i] = MAX_PERMUTATION_SIZE + 1;
+		        }
+		}
+	}
+
+	inline std::vector<std::vector<int>> generatePermutations(int n) {
+		std::vector<int> ints(n);
+		std::iota (std::begin(ints), std::end(ints), 1);
+		std::vector<std::vector<int>> out;
+		std::vector<int> vec(n, MAX_PERMUTATION_SIZE + 1);
+
+		backtrackPermutations(ints, vec, 0, out);
+
+		return out;
 	}
 }
