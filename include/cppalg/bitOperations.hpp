29f15f7117071239367c7c18d33b1d26f5a3b966
kasperiapell <kasperi.apell@gmail.com>
2023-09-23 21:58:26 +0300

    add hamming weight

--- include/cppalg/bitOperations.hpp
+++ include/cppalg/bitOperations.hpp
@@ -1,6 +1,7 @@
 // include/cppalg/bitOperations.hpp
 
 namespace binaryTree {
+
 	int hammingDistance(int x, int y) {
 		int z = x ^ y;
 		int out = z & 1;
@@ -12,4 +13,18 @@ namespace binaryTree {
 
 		return out;
 	}
+
+	// Computes the number of 1 bits in a 32-bit unsigned integer
+	int hammingWeight(uint32_t n) {
+		uint32_t count = 0;
+		uint32_t mask = 1;
+
+		while (n) {
+			if (n & mask) count++;
+			n >>= 1;
+		}
+
+		return count;
+	}
+
 }
