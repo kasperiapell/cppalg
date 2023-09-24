bc4d618b9fd85fe1df24a223e43b836aee3b8fbe
kasperiapell <kasperi.apell@gmail.com>
2023-09-24 20:58:42 +0300

    add bit reversal

--- include/cppalg/bitOperations.hpp
+++ include/cppalg/bitOperations.hpp
@@ -15,7 +15,7 @@ namespace binaryTree {
 	}
 
 	// Computes the number of 1 bits in a 32-bit unsigned integer
-	int hammingWeight(uint32_t n) {
+	uint32_t hammingWeight(uint32_t n) {
 		uint32_t count = 0;
 		uint32_t mask = 1;
 
@@ -25,6 +25,20 @@ namespace binaryTree {
 		}
 
 		return count;
+	}
+
+	// Reverses the bits of a 32-bit unsigned integer
+	uint32_t reverseBits(uint32_t n) {
+		uint32_t out;
+
+	        for (int i = 0; i < 32; i++) {
+			out = out << 1;
+			bool temp = n & 1;
+			out |= temp;
+			n = n >> 1;
+	        }
+
+	        return out;
 	}
 
 }
